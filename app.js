dd2a35b03c4f6bcdb473e9d879ad53738c09fcdb
abdulahad0110 <abdulahad200325@gmail.com>
2024-04-30 23:39:50 +0500

    string methods

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 
 
 document.write("<h2>Task 01</h2>");
+
+
 var firstName = prompt("Enter first name: ");
+
 var lastName = prompt("Enter last name: ");
+
 document.write(`Asalamualikum, ${firstName} ${lastName}.`);
 
 
@@ -11,10 +15,16 @@ document.write(`Asalamualikum, ${firstName} ${lastName}.`);
 
 
 document.write("<h2>Task 02</h2>");
+
+
 var phoneModel = prompt("Whats your favourite mobile phone model");
+
 document.write("My favorite phone is: " + phoneModel);
+
 var phoneModelLen = phoneModel.length
-document.write("<br>")
+
+document.write("<br>");
+
 document.write("Lenght of string is: " + phoneModelLen );
 
 
@@ -22,11 +32,18 @@ document.write("Lenght of string is: " + phoneModelLen );
 
 
 document.write("<h2>Task 03</h2>");
+
+
 var word = "Pakistan"
+
 var letter = prompt("String: '" + word + "' Find the index of letter: ");
+
 var letterIndex = word.indexOf(letter);
+
 document.write("String: " + word);
+
 document.write("<br>");
+
 document.write("Index of '" + letter + "' : " + letterIndex);
 
 
@@ -34,11 +51,18 @@ document.write("Index of '" + letter + "' : " + letterIndex);
 
 
 document.write("<h2>Task 04</h2>");
+
+
 var word = "Hello, World!!!"
+
 var letter = prompt("String: '" + word + "' Find the index of last letter: ");
+
 var letterLastIndex = word.lastIndexOf(letter);
+
 document.write("String: " + word);
+
 document.write("<br>");
+
 document.write("Index of last letter '" + letter + "' : " + letterLastIndex);
 
 
@@ -46,10 +70,16 @@ document.write("Index of last letter '" + letter + "' : " + letterLastIndex);
 
 
 document.write("<h2>Task 05</h2>");
+
+
 var word = "Pakistan"
+
 var thirdIndexLetter = word.charAt(3);
+
 document.write("String: " + word);
+
 document.write("<br>");
+
 document.write("Character at index 3 is : " + thirdIndexLetter);
 
 
@@ -57,10 +87,16 @@ document.write("Character at index 3 is : " + thirdIndexLetter);
 
 
 document.write("<h2>Task 06</h2>");
+
+
 var firstName = "Abdul"
+
 var lastName = "Ahad"
+
 var fullName = firstName.concat(' ', lastName);
+
 var greeting = fullName.concat(', ', 'Asalam-u-alailum');
+
 document.write(greeting);
 
 
@@ -68,10 +104,15 @@ document.write(greeting);
 
 
 document.write("<h2>Task 07</h2>");
+
 var cityName = "Hyderabad"
+
 var cityName1 = cityName.replace('Hyder', 'Islam');
+
 document.write("City : " + cityName);
+
 document.write("<br>");
+
 document.write("After replacement: " + cityName1);
 
 
@@ -79,12 +120,20 @@ document.write("After replacement: " + cityName1);
 
 
 document.write("<h2>Task 08</h2>");
-var message = "Ali and Sami are best friends. They play circket and football together."
+
+
+var message = "Ali and Sami are best friends. They play circket and football together.";
+
 document.write("String: " + message);
+
 document.write("<br>");
+
 document.write("After replace 'and' in the string with '&'");
+
 message = message.replaceAll('and', '&');
+
 document.write("<br>");
+
 document.write("String: " + message);
 
 
@@ -92,14 +141,21 @@ document.write("String: " + message);
 
 
 document.write("<h2>Task 09</h2>");
+
 var value = "472"
+
 document.write("Value: " + value);
+
 document.write("<br>");
-document.write("Type: " + typeof value)
+
+document.write("Type: " + typeof value);
 
 value = parseInt(value);
 
 document.write("<br>");
+
 document.write("Value: " + value);
+
 document.write("<br>");
-document.write("Type: " + typeof value)
+
+document.write("Type: " + typeof value);
